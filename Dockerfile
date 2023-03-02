0ac7e874bcdd9844aee44c0786baacc1a22b9960
Adarsh Prasad <adarsh.prasad@davita.com>
2023-03-02 16:43:48 +0530

    push code

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openkbs/jdk11-mvn-py3
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/com/javatechyonly/model/Book.java
@@ -0,0 +1,17 @@
+package com.javatechyonly.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "Book")
+public class Book {
+
+	@Id
+	private int id;
+	private String bookName;
+	private String authorName;
+	private String description;
+}
